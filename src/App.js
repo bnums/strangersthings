9ecf434aaf033b8e99f18f8c609e46ce46343504
Bryan Num <bnum33@gmail.com>
2022-02-28 10:05:58 -0800

    finished project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom"
-import { useEffect, useState } from 'react';
-import { callApi } from './api';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { callApi } from "./api";
 
 // React components
 import {
@@ -13,39 +12,39 @@ import {
   Home,
   MessageForm,
   EditForm,
-} from './components';
-
+} from "./components";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [token, setToken] = useState('');
-  const [user, setUser] = useState('guest');
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState("guest");
   const [messages, setMessages] = useState([]);
 
-
   const fetchPosts = async () => {
-    const { data: { posts } } = await callApi({ url: '/posts', token })
+    const {
+      data: { posts },
+    } = await callApi({ url: "/posts", token });
     if (posts) {
       setPosts(posts);
     }
-  }
+  };
 
   const deletePost = async (id) => {
     try {
-      await callApi({ url: `/posts/${id}`, method: 'DELETE', token })
-      const newPosts = posts.filter(element => element._id !== id);
+      await callApi({ url: `/posts/${id}`, method: "DELETE", token });
+      const newPosts = posts.filter((element) => element._id !== id);
       setPosts(newPosts);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      setToken(localStorage.getItem('token')) //when user logged in set localstorage token so user will be logged in
-      setUser(localStorage.getItem('user'))
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token")); //when user logged in set localstorage token so user will be logged in
+      setUser(localStorage.getItem("user"));
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     try {
@@ -57,34 +56,91 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
-
   return (
     <div className="App">
-      <Navigation user={user}/>
-      {token &&
+      <Navigation user={user} />
+      {token && (
         <>
           <h2> Welcome {user}! </h2>
           <button
             onClick={() => {
-              setToken('');
-              setUser('guest');
-              setMessages('');
+              setToken("");
+              setUser("guest");
+              setMessages("");
               localStorage.clear();
-            }
-            }>Log Out</button>
+            }}
+          >
+            Log Out
+          </button>
         </>
-      }
+      )}
       <Routes>
-        <Route exact path='/account/:method' element={<AccountForm setUser={setUser} setToken={setToken} setMessages={setMessages} />} />
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/posts' element={<Posts setPosts={setPosts} posts={posts} fetchPosts={fetchPosts} token={token} user={user} deletePost={deletePost} />} />
-        <Route path='/profile/:user' element={<Profile token={token} posts={posts} setPosts={setPosts} messages={messages} setMessages={setMessages} deletePost={deletePost} user={user}/>} />
-        <Route exact path='/posts/:postId/messages' element={<MessageForm token={token} setPosts={setPosts} fetchPosts={fetchPosts} posts={posts} />} />
-        <Route path='/posts/:postId/edit' element={<EditForm posts={posts} setPosts={setPosts} token={token} fetchPosts={fetchPosts}/>} />
+        <Route
+          exact
+          path="/account/:method"
+          element={
+            <AccountForm
+              setUser={setUser}
+              setToken={setToken}
+              setMessages={setMessages}
+            />
+          }
+        />
+        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/posts"
+          element={
+            <Posts
+              setPosts={setPosts}
+              posts={posts}
+              fetchPosts={fetchPosts}
+              token={token}
+              user={user}
+              deletePost={deletePost}
+            />
+          }
+        />
+        <Route
+          path="/profile/:user"
+          element={
+            <Profile
+              token={token}
+              posts={posts}
+              setPosts={setPosts}
+              messages={messages}
+              setMessages={setMessages}
+              deletePost={deletePost}
+              user={user}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/posts/:postId/messages"
+          element={
+            <MessageForm
+              token={token}
+              setPosts={setPosts}
+              fetchPosts={fetchPosts}
+              posts={posts}
+            />
+          }
+        />
+        <Route
+          path="/posts/:postId/edit"
+          element={
+            <EditForm
+              posts={posts}
+              setPosts={setPosts}
+              token={token}
+              fetchPosts={fetchPosts}
+            />
+          }
+        />
       </Routes>
-    </div >
+    </div>
   );
 }
-
 
 export default App;
